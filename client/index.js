8b5e17866a110490003640c6527c1519e4536d46
Schaaz <113603403+Schaaz@users.noreply.github.com>
2023-02-10 19:49:31 +0530

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+var timerObj = document.getElementById("timer")
+var startBtnObj = document.getElementById("start-btn")
+var resetBtnObj = document.getElementById("reset-btn")
+var isTimerRunning = false
+var interval
+resetBtnObj.addEventListener("click",function(){
+    console.log("reset button clicked");
+    timerObj.textContent = "00:00";
+    
+})
+startBtnObj.addEventListener("click",()=>{
+    console.log("start button clicked");
+    if (isTimerRunning){
+        startBtnObj.textContent = "Start";
+        isTimerRunning = false
+        clearInterval(interval)
+    } else {
+        startBtnObj.textContent="Stop";
+        isTimerRunning = true
+        interval = setInterval(function(){
+            console.log("hi")
+        }, 1000)
+
+    }
+    
+    
+})
